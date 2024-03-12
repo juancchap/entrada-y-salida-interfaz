23ee44171d6e26ddaffa0346ade3d6d1a8a95781
juanc <juanc@Juan_Carlos>
2024-03-12 12:20:29 -0600

    crea un programa en java que nos pida 1 numero y el numero que introdusimos nos lo reflege en un tex filed

---
+++ src/entradaysalida/EntradaYSalida.java
@@ -0,0 +1,14 @@
+
+package entradaysalida;
+
+
+public class EntradaYSalida {
+
+    public static void main(String[] args) {
+       
+        Interfaz entrada = new Interfaz();
+        entrada.setVisible(true);
+        entrada.setLocationRelativeTo(null);
+    }
+    
+}
